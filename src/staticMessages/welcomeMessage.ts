4d208583e4af93395d84d029307673e78bd37a9f
Srijan Personal <chapssrijan@gmail.com>
2024-08-12 19:48:31 +0300

    text updated

--- src/staticMessages/welcomeMessage.ts
+++ src/staticMessages/welcomeMessage.ts
@@ -15,5 +15,12 @@ Hello! I'm Srijan, and I'm excited to share my work with you. Below is a little 
 </pre>
 </div>
 
-Feel free to explore the project and reach out if you have any questions! Start with \`help\` command?
+What does this terminal support?
+
+1. Few of the native <code>zsh</code> terminal features to lighten your mood.
+2. Navigate through <code>history</code> of commands used using up and down arrow.
+3. Still using <code>cat</code> is not too outdated, right?
+4. Customize your terminal with top right setting icon, mine favourite is <code>solarized</code>.
+
+And many more to follow in the near future :) Feel free to explore the project and reach out if you have any questions! Start with \`help\` command?
 `);

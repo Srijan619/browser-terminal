36ee6e3c88f02353fbbf02347f4bf8decb5e0dab
Srijan Personal <chapssrijan@gmail.com>
2024-10-02 21:09:44 +0300

    format commandpromptinput file

--- src/components/CommandPromptInput.vue
+++ src/components/CommandPromptInput.vue
@@ -1,108 +1,122 @@
 <script setup lang="ts">
-import { ref, defineProps, onMounted, onUnmounted } from 'vue';
-import { useCommandPromptStore } from '../stores/globalStore';
-import { sendInputToServer } from '../utils/commandsToServer';
-
+import { ref, defineProps, onMounted, onUnmounted } from 'vue'
+import { useCommandPromptStore } from '../stores/globalStore'
+import { sendInputToServer } from '../utils/commandsToServer'
 
 const props = defineProps<{
-    id: string,
-    enabled: boolean;
-}>();
+    id: string
+    enabled: boolean
+}>()
 
-const store = useCommandPromptStore();
-const PROMPT_INSTANCE = store.PROMPT_INSTANCES.find(instance => instance.id === props.id);
-const currentCommand = ref(PROMPT_INSTANCE?.command || '');
+const store = useCommandPromptStore()
+const PROMPT_INSTANCE = store.PROMPT_INSTANCES.find(
+    (instance) => instance.id === props.id
+)
+const currentCommand = ref(PROMPT_INSTANCE?.command || '')
 
 // Create a reference for the input
-const commandInput = ref<HTMLInputElement | null>(null);
+const commandInput = ref<HTMLInputElement | null>(null)
 
-let debounceTimeout: number | undefined;
+let debounceTimeout: number | undefined
 
 // Debounce function
 const debounce = (func: () => void, delay: number) => {
     return () => {
-        clearTimeout(debounceTimeout);
+        clearTimeout(debounceTimeout)
         debounceTimeout = window.setTimeout(() => {
-            func();
-        }, delay);
-    };
-};
+            func()
+        }, delay)
+    }
+}
 // Focus the input on component mount
 onMounted(() => {
     // As we constantly put focus to input, add selection change to allow user to select text if it is needed
-    document.addEventListener('selectionchange', handleSelectionChange);
-    focusInput();
-});
+    document.addEventListener('selectionchange', handleSelectionChange)
+    focusInput()
+})
 
 // Cleanup event listeners
 onUnmounted(() => {
-    document.removeEventListener('selectionchange', handleSelectionChange);
-});
-
+    document.removeEventListener('selectionchange', handleSelectionChange)
+})
 
 // Function to focus the input
 const focusInput = () => {
-    commandInput.value?.focus();
-};
+    console.log('focuse input...')
+    commandInput.value?.focus()
+}
 
 const blurInput = () => {
-    commandInput.value?.blur();
-};
+    commandInput.value?.blur()
+}
 
 // Handle command input enter
 const handleEnter = () => {
     if (store.TERMINAL_MODE === 'client') {
-        console.log("Client mode..")
-        store.handleCommandInputEnter(props.id, currentCommand.value);
+        console.log('Client mode..')
+        store.handleCommandInputEnter(props.id, currentCommand.value)
     } else {
-        console.log("Server mode..")
+        console.log('Server mode..')
         if (PROMPT_INSTANCE) {
             sendInputToServer(currentCommand.value, PROMPT_INSTANCE)
         }
     }
-};
+}
 
 // Handle selection change
 const handleSelectionChange = debounce(() => {
-    const selection = window.getSelection();
+    const selection = window.getSelection()
     if (selection && selection.toString().length > 0) {
-        blurInput();
+        blurInput()
     } else {
-        focusInput();
+        focusInput()
     }
-}, 300); // Adjust the delay as needed
+}, 300) // Adjust the delay as needed
 
-const HISTORY_COUNTER = ref(-1);
+const HISTORY_COUNTER = ref(-1)
 
 const handleShowHistoryUp = () => {
-    if (store.COMMAND_HISTORY.length === 0) return; // No history available
+    if (store.COMMAND_HISTORY.length === 0) return // No history available
 
-    blurInput();
+    blurInput()
     // Move up in history
     if (HISTORY_COUNTER.value < store.COMMAND_HISTORY.length - 1) {
-        HISTORY_COUNTER.value++;
-        currentCommand.value = store.COMMAND_HISTORY[store.COMMAND_HISTORY.length - 1 - HISTORY_COUNTER.value].toString();
+        HISTORY_COUNTER.value++
+        currentCommand.value =
+            store.COMMAND_HISTORY[
+                store.COMMAND_HISTORY.length - 1 - HISTORY_COUNTER.value
+            ].toString()
     }
-};
+}
 
 const handleShowHistoryDown = () => {
     if (store.COMMAND_HISTORY.length === 0 || HISTORY_COUNTER.value <= 0) {
-        return; // Do nothing if no history has been traversed or reached the end
+        return // Do nothing if no history has been traversed or reached the end
     }
 
-    blurInput();
+    blurInput()
     // Move down in history
-    currentCommand.value = store.COMMAND_HISTORY[store.COMMAND_HISTORY.length - HISTORY_COUNTER.value].toString();
-    HISTORY_COUNTER.value--;
-};
-
-
+    currentCommand.value =
+        store.COMMAND_HISTORY[
+            store.COMMAND_HISTORY.length - HISTORY_COUNTER.value
+        ].toString()
+    HISTORY_COUNTER.value--
+}
 </script>
 
 <template>
-    <input ref="commandInput" class="command-prompt" v-model="currentCommand" autocorrect="off" autocapitalize="off"
-        spellcheck="false" @keyup.enter="handleEnter" @keyup.up.prevent="handleShowHistoryUp"
-        @keyup.down.prevent="handleShowHistoryDown" :disabled="!props.enabled" />
+    <input
+        ref="commandInput"
+        class="command-prompt"
+        v-model="currentCommand"
+        autocorrect="off"
+        autocapitalize="off"
+        spellcheck="false"
+        @keyup.enter="handleEnter"
+        @keyup.up.prevent="handleShowHistoryUp"
+        @keyup.down.prevent="handleShowHistoryDown"
+        :disabled="!props.enabled"
+    />
 </template>
 
 <style scoped>
